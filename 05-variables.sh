2bbc872ebce629f7f3ded19acc7747343873734a
sidhartha <sidharthakumaras@gmail.com>
2023-09-16 15:48:19 +0530

    date commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,12 @@
 
 echo "the user is"
 
-read USERNAME
+read -s USERNAME
 
 echo "user is : $USERNAME"
+
+echo "the pwd is"
+
+read -s PASSWORD # -s --> no one will see what we are entering
+
+echo "pwd is : $PASSWORD"
