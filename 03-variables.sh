dd521eba1f9dbce3d45cf94c531e981351d11a8d
sidhartha <sidharthakumaras@gmail.com>
2023-09-16 15:17:41 +0530

    date commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-person1=$1;
-person2=$2;
+person1=$1; # this is the first value from the command line
+person2=$2; # this is the second value from the command line
 
 echo "$person1:Hey $person2"
 echo "$person2:Hey $person1"
---
+++ 04-variablesAdd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUM1=$1
+NUM2=$2
+
+#I need to run the command inside the shell
+sum=$((NUM1+NUM2))
+
+echo "the sum of two numbers is : $sum"
