51ff07e77af76eb78820e96a26ef6333430f2702
sidhartha <sidharthakumaras@gmail.com>
2023-09-20 00:30:37 +0530

    special variables

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -3,9 +3,12 @@
 VAR1=$1
 VAR2=$2
 
+# to know the script name 
+echo "ScriptName : $0"
+
 echo "1st variable : $VAR1"
 # @ --> all
 echo "all var : $@"
 
-# - means how many varibals are passed
+# - means gives count how many varibals are passed
 echo "how many variabes are there : $#"
