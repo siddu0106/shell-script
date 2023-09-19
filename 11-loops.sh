9bf6f24b9aebc6742e9343226e9618742a92d7f5
sidhartha <sidharthakumaras@gmail.com>
2023-09-20 01:34:19 +0530

    loops

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+VALIDATE()
+{
+    #$1 --> It will receive the argument 1
+    #$2 --> argument 2 --> to know the name
+    if [ $1 -ne 0 ]
+    then
+        echo  "ERROR : $2 is not success"
+        exit 1
+    else 
+        echo "SUCCESS : $2 is success"
+    fi
+}
+
 #print 1-100 numbers
 
 for i in {1..100}
 do
     echo "$i"
 done
+
+#all arguments are in $@
+for j in $@
+do
+    yum install $j -y &>>$LOGFILE
+    VALIDATE $? $j
+done
+
