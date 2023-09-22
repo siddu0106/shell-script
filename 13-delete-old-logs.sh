5fc896a91ee00792d3f5049595da0a12eef09ada
sidhartha <sidharthakumaras@gmail.com>
2023-09-22 12:26:38 +0530

    delete-old-files

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -8,3 +8,11 @@ LOGFILE=/root/shell-script/shelscript-logs/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+# it will read the inputs from FILES_TO_DELETE variable and read 
+# every line and store in line variable 
+while read line
+do
+    echo "Deleting and redirecting $line" &>>$LOGFILE
+    rm -rf $line
+done >>> $FILES_TO_DELETE
